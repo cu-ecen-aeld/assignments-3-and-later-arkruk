d491ba6eac246d41bd2c077270545a354b479141
kruq <kruq@ubuntu.myguest.virtualbox.org>
2024-07-14 15:01:33 +0200

    Add better error handling

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[])
 {
     int fd;
+    int result;
     if (argc == 1)
     {
         syslog(LOG_ERR, "path and text string not defined");
@@ -22,29 +23,23 @@ int main(int argc, char *argv[])
     else
     {
         fd = open(argv[1], O_CREAT|O_RDWR, S_IROTH | S_IWOTH | S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP);
-        write(fd, argv[2], strlen(argv[2]));
-        close(fd);
-        syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
+        if(fd < 0)
+        {
+            syslog(LOG_ERR, "Cannot open or create file %s", argv[1]);
+            return 1;
+        }
+        else
+        {
+            result = write(fd, argv[2], strlen(argv[2]));
+            if(result < 0)
+            {
+                syslog(LOG_ERR, "Cannot write to file %s", argv[1]);
+                close(fd);
+                return 1;
+            }
+            close(fd);
+            syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
+        }
     }
     return 0;
 }
-
-/*
-if [ $# -eq 0 ]; then
-    echo "path and text string not defined"
-    exit 1
-elif [ $# -eq 1 ]; then
-    echo "text string not defined"
-    exit 1
-else
-    dir_name=$(dirname $1)
-    mkdir -p $dir_name
-    echo "$2" > $1
-    if [ -f $1 ]; then
-        exit 0
-    else
-        echo "file is not created"
-        exit 1
-    fi
-fi
-*/
