e0fd4f53f87fa753dfa8b06179bbfa54b2d73fd1
kruq <kruq@kruq.myguest.virtualbox.org>
2024-07-20 20:34:50 +0200

    threading implementation

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,24 +10,42 @@
 
 void* threadfunc(void* thread_param)
 {
+    int result;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success = false;
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    result = pthread_mutex_lock(thread_func_args->mutex);
+    if (result != 0)
+    {
+        ERROR_LOG("Mutex lock failed with result %d",result);
+        return thread_func_args;
+    }
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    result = pthread_mutex_unlock(thread_func_args->mutex);
+    if (result != 0)
+    {
+        ERROR_LOG("Mutex unlock failed with result %d",result);
+        return thread_func_args;
+    }
+    thread_func_args->thread_complete_success = true;
+    return thread_func_args;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+    struct thread_data* thread_data_ = malloc(sizeof(struct thread_data));
+    thread_data_->mutex = mutex;
+    thread_data_->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_data_->wait_to_release_ms = wait_to_release_ms;
+
+    int result = pthread_create(thread, NULL, threadfunc, (void*)thread_data_);
+    if (result != 0)
+    {
+        ERROR_LOG("Cannot create thread with result %d",result);
+        return false;
+    }
+    return true;
 }
 
